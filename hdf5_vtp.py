c3106f9c5ec7cfa603403505d02e4957a7d8eee6
Ezra Tjung <ezrayst@berkeley.edu>
2018-09-20 00:18:21 -0700

    :art: use VTP

--- hdf5_vtp.py
+++ hdf5_vtp.py
@@ -1,10 +1,12 @@
 # Import libraries
-from evtk.hl import unstructuredGridToVTK 
-from evtk.hl import pointsToVTK 
+import datetime
 import matplotlib as mp
 import numpy as np
 import pandas as pd
 import os
+
+# Import external libraries
+from evtk.hl import pointsToVTK 
 
 # Specify directories
 working_directory = 'bin/20180918_columncollapse_usf/'
@@ -14,11 +16,12 @@ input_filename_prefix = 'particles00'
 input_filename_suffix = '00.h5'
 
 # Output files
-output_directory = working_directory + 'vtk/'
+output_directory = working_directory + 'vtp/'
 output_prefix_filename = 'stress'
-output_suffix_filename = '.vtk'
+output_suffix_filename = '.vtp'
 if not os.path.exists(output_directory):
 	os.makedirs(output_directory)
+print("directory of files: " + output_directory)
 
 # Loop all the .h5 files
 ntime = 5
@@ -36,13 +39,25 @@ for index in range(0, ntime, 1):
 	coord_z = np.array(df['coord_z'])
 	stress_xx = np.array(df['stress_xx'])
 	stress_yy = np.array(df['stress_yy'])
-	conn = np.zeros(len(coord_x))
-	celltype = np.zeros(len(coord_x))
-	offset = np.zeros(len(coord_x))
+	stress_zz = np.array(df['stress_zz'])
 
-	# Write VTK file
-	unstructuredGridToVTK(output_filename, coord_x, coord_y, coord_z, connectivity = conn, offsets = offset, cell_types = celltype, cellData = {"stress_xx" : stress_xx }, pointData = {"stress_yy" : stress_yy})
+	# Make data to store
+	data = np.empty((len(coord_x), np.zeros(3)))
+	iterate = 1
+	for iterate in range(1, len(coord_x)):
+		data[iterate][1] = stress_xx[iterate]
+		data[iterate][2] = stress_yy[iterate]
+		data[iterate][3] = stress_zz[iterate]
+		# Update iterate
+		iterate += 1
+	print(data.shape)
+
+	# Write VTP file
+	pointsToVTK(output_filename, coord_x, coord_y, coord_z, data = {output_prefix_filename : data})
 	
+	# Print prompts
+	print(output_prefix_filename + str(index) + output_suffix_filename + " has been processed at " + str(datetime.datetime.now()))
+
 	# Update index for next time step
 	index += 1
 

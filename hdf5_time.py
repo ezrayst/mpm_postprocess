17facf82e2d856d4f2b5c82f05f69f36506ac0c0
Ezra <ezrayst@berkeley.edu>
2018-11-28 16:10:53 -0800

    :wrench: add feathres

--- hdf5_time.py
+++ hdf5_time.py
@@ -7,77 +7,99 @@ import pandas as pd
 import os
 
 # Specify directories
-working_directory = 'bin/hydrostatic_usf_linearelastic-1/'
-
+working_directory = ['bin/hydrostatic_usf_newtonian-1/',
+                     'bin/hydrostatic_usl_newtonian-1/',
+                     'bin/hydrostatic_usf_newtonian-2/',
+                     'bin/hydrostatic_usl_newtonian-2/',
+                     'bin/hydrostatic_usf_newtonian-2/',
+                     'bin/hydrostatic_usl_newtonian-2/']
 # Input Files
 input_filename_prefix = 'particles'
 input_filename_suffix = '00.h5'
 
-# Output files
-output_directory = working_directory + 'results/'
-output_prefix_filename = 'stress'
-output_suffix_filename = '.vtp'
-if not os.path.exists(output_directory):
-	os.makedirs(output_directory)
-print("directory of files: " + output_directory)
-
 # Add input
 ntime = 99
-dt = 0.00001
+dt = 0.001
 point_id = [0];
 
 # Preallocate variables
 time = np.zeros((ntime + 1, 1))
-stress = np.zeros((ntime + 1, len(point_id)))
+stress = np.zeros((ntime + 1, len(working_directory)))
 
-# Loop all the .h5 files
-for index in range(0, ntime + 1, 1):
+for k in range(0, len(working_directory), 1):
 
-	# Prefix number of input file
-	if index < 10:
-		zeros = '00'
-	elif index < 100:
-		zeros = '0'
-	else:
-		zeros = ''
+	# Output files
+	output_directory = working_directory[k] + 'results/'
+	output_prefix_filename = 'stress'
+	output_suffix_filename = '.vtp'
+	if not os.path.exists(output_directory):
+		os.makedirs(output_directory)
+	print("directory of files: " + output_directory)
+	
+	# Loop all the .h5 files
+	for index in range(0, ntime + 1, 1):
+	
+		# Index multiplication
+		if k < 2:
+			multiplication = 1
+		elif k < 4:
+			multiplication = 2
+		else:
+			multiplication = 2
 
-	# Concatenate filename
-	input_filename = working_directory + input_filename_prefix + zeros + str(index) + input_filename_suffix
-	output_filename = output_directory + output_prefix_filename + str(index)
+		index_mult = index * multiplication;
 
-	# Read HDF5 - df refers to DataFrame
-	df = pd.read_hdf(input_filename)
+		# Prefix number of input file
+		if index_mult < 10:
+			zeros = '00'
+		elif index_mult < 100:
+			zeros = '0'
+		else:
+			zeros = ''
 
-	# Make np.array
-	#coord_x = np.array(df['coord_x'])
-	#coord_y = np.array(df['coord_y'])
-	#coord_z = np.array(df['coord_z'])
-	#stress_xx = np.array(df['stress_xx'])
-	stress_yy = np.array(df['stress_yy'])
-	#stress_zz = np.array(df['stress_zz'])
-	#tau_xy = np.array(df['tau_xy'])
-	#strain_xx = np.array(df['strain_xx'])
-	#strain_yy = np.array(df['strain_yy'])
-	#strain_zz = np.array(df['strain_zz'])
-
-	# Make data to store
-	for j in range(0, len(point_id), 1):
-		stress[index, j] = stress_yy[point_id[j]]	
-
-	# Prompt to make sure it's OK
-	print(input_filename + " has been read at " + str(datetime.datetime.now()))
-
-	# Update time
-	time[index] = index * dt;
-
-	# Update index for next time step
-	index += 1
+		# Concatenate filename
+		input_filename = working_directory[k] + input_filename_prefix + zeros + str(index_mult) + input_filename_suffix
+		output_filename = output_directory + output_prefix_filename + str(index_mult)
+	
+		# Read HDF5 - df refers to DataFrame
+		df = pd.read_hdf(input_filename)
+	
+		# Make np.array
+		#coord_x = np.array(df['coord_x'])
+		#coord_y = np.array(df['coord_y'])
+		#coord_z = np.array(df['coord_z'])
+		#stress_xx = np.array(df['stress_xx'])
+		stress_yy = np.array(df['stress_yy'])
+		#stress_zz = np.array(df['stress_zz'])
+		#tau_xy = np.array(df['tau_xy'])
+		#strain_xx = np.array(df['strain_xx'])
+		#strain_yy = np.array(df['strain_yy'])
+		#strain_zz = np.array(df['strain_zz'])
+	
+		# Make data to store
+		for j in range(0, len(point_id), 1):
+			stress[index, k] = stress_yy[point_id[j]]	
+	
+		# Prompt to make sure it's OK
+		print(input_filename + " has been read at " + str(datetime.datetime.now()))
+	
+		# Update time
+		time[index] = index * dt;
+	
+		# Update index for next time step
+		index += 1
 
 # Plot
-plt.plot(time, stress, 'b')
-plt.axis([0, 1, -30000, 0])
+line1, = plt.plot(time, stress[:, [0]], 'r', label='USF Case 1')
+line2, = plt.plot(time, stress[:, [1]], 'c--', label='USL Case 1')
+line3, = plt.plot(time, stress[:, [2]], 'g', label='USF Case 2')
+line4, = plt.plot(time, stress[:, [3]], 'm--', label='USL Case 2')
+line5, = plt.plot(time, stress[:, [4]], 'g', label='USF Case 3')
+line6, = plt.plot(time, stress[:, [5]], 'y--', label='USL Case 3')
+plt.axis([0, 0.1, -2500, 0])
 plt.xlabel('Time (s)')
-plt.ylabel('Stress (Pa)')
+plt.ylabel('Stress in Y direction (Pa)')
+plt.legend(handles=[line1, line2, line3, line4, line5, line6])
 plt.show()
 
 
